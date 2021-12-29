643014ed59564fe0620e9f17f20c012d732fc5e6
abdelwahab <abdelwahab20191702045@cis.asu.edu.eg>
2021-12-29 20:16:52 +0200

    Add project files.

---
+++ HTTPServer/Request.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace HTTPServer
+{
+    public enum RequestMethod
+    {
+        GET,
+        POST,
+        HEAD
+    }
+
+    public enum HTTPVersion
+    {
+        HTTP10,
+        HTTP11,
+        HTTP09
+    }
+
+    class Request
+    {
+        string[] requestLines;
+        RequestMethod method;
+        public string relativeURI;
+        Dictionary<string, string> headerLines;
+
+        public Dictionary<string, string> HeaderLines
+        {
+            get { return headerLines; }
+        }
+
+        HTTPVersion httpVersion;
+        string requestString;
+        string[] contentLines;
+        string[] reqList;
+
+        public Request(string requestString)
+        {
+            this.requestString = requestString;
+        }
+        /// <summary>
+        /// Parses the request string and loads the request line, header lines and content, returns false if there is a parsing error
+        /// </summary>
+        /// <returns>True if parsing succeeds, false otherwise.</returns>
+        public bool ParseRequest()
+        {
+
+            String[] separators = new String[] { "\r\n" };
+
+            //TODO: parse the receivedRequest using the \r\n delimeter
+
+            requestLines = requestString.Split(separators, 10, StringSplitOptions.None);
+
+            // check that there is atleast 3 lines: Request line, Host Header, Blank line (usually 4 lines with the last empty line for empty content)
+
+            // Parse Request line
+
+            if (!ParseRequestLine())
+            {
+                return false;
+            }
+
+            if (!ValidateIsURI(relativeURI))
+            {
+
+                return false;
+            }
+
+            // Validate blank line exists
+            if (!ValidateBlankLine())
+            {
+                return false;
+            }
+
+            // Load header lines into HeaderLines dictionary
+
+            if (!LoadHeaderLines())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ParseRequestLine()
+        {
+            //throw new NotImplementedException();
+            reqList = requestLines[0].Split(' ');
+
+            String Req_method = reqList[0];
+
+            relativeURI = reqList[1];
+
+            String version = reqList[2];
+
+            if (requestLines.Length < 3)
+            {
+                return false;
+            }
+
+            switch (version)
+            {
+                case "HTTP/0.9":
+
+                    httpVersion = HTTPVersion.HTTP09;
+
+                    break;
+                case "HTTP/1.1":
+
+                    httpVersion = HTTPVersion.HTTP11;
+
+                    break;
+                case "HTTP/1.0":
+
+                    httpVersion = HTTPVersion.HTTP10;
+                    break;
+                default:
+                    httpVersion = HTTPVersion.HTTP09;
+                    break;
+            }
+
+            // Choosing method type
+            switch (Req_method)
+            {
+                case "GET":
+                    method = RequestMethod.GET;
+                    break;
+
+                case "POST":
+                    method = RequestMethod.POST;
+                    break;
+
+                case "HEAD":
+                    method = RequestMethod.HEAD;
+                    break;
+                default:
+                    Console.WriteLine("Method Error!!");
+                    break;
+            }
+            return true;
+        }
+
+        private bool ValidateIsURI(string uri)
+        {
+            return Uri.IsWellFormedUriString(uri, UriKind.RelativeOrAbsolute);
+        }
+
+        private bool LoadHeaderLines()
+        {
+            //throw new NotImplementedException();
+
+            String[] headerLinesArr = new String[100];
+
+            for (int i = 1; i < requestLines.Length; i++)
+            {
+                if (requestLines[i] == "\r\n\r\n")
+                {
+                    break;
+                }
+                else
+                {
+                    headerLinesArr[i - 1] = requestLines[i];
+                }
+            }
+
+
+            String[] temp = new String[2];
+            String[] separator = new String[] { ": " };
+            for (int i = 0; i < headerLinesArr.Length; i++)
+            {
+                temp = headerLinesArr[i].Split(separator, 10, StringSplitOptions.None);
+                headerLines.Add(temp[0], temp[1]);
+            }
+
+            if (headerLines.Count != headerLinesArr.Length)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidateBlankLine()
+        {
+
+            if (!requestString.Contains("\r\n\r\n"))
+            {
+                return false;
+            }
+            //throw new NotImplementedException();
+            return true;
+        }
+
+    }
+}
---
+++ HTTPServer/Server.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace HTTPServer
+{
+    class Server
+    {
+        Socket serverSocket;
+
+        public Server(int portNumber, string redirectionMatrixPath)
+        {
+            //TODO: call this.LoadRedirectionRules passing redirectionMatrixPath to it
+            //TODO: initialize this.serverSocket
+            this.LoadRedirectionRules(redirectionMatrixPath);
+            this.serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        }
+
+        public void StartServer()
+        {
+            // TODO: Listen to connections, with large backlog.
+            serverSocket.Listen(100);
+
+            // TODO: Accept connections in while loop and start a thread for each connection on function "Handle Connection"
+            while (true)
+            {
+                //TODO: accept connections and start thread for each accepted connection.
+                Socket clientSocket = serverSocket.Accept();
+                Thread newThread = new Thread(new ParameterizedThreadStart(HandleConnection));
+                newThread.Start(clientSocket);
+            }
+        }
+
+        public void HandleConnection(object obj)
+        {
+            // TODO: Create client socket 
+            // set client socket ReceiveTimeout = 0 to indicate an infinite time-out period
+            Socket clientSocket = (Socket)obj;
+            clientSocket.ReceiveTimeout = 0;
+
+            // TODO: receive requests in while true until remote client closes the socket.
+            while (true)
+            {
+                try
+                {
+                    // TODO: Receive request
+                    byte[] data = new byte[1024];
+                    int receivedDataLength = clientSocket.Receive(data);
+                    // TODO: break the while loop if receivedLen==0
+                    if (receivedDataLength == 0)
+                    {
+                        Console.WriteLine("Client didn't send anything");
+                        break;
+                    }
+                    // TODO: Create a Request object using received request string
+                    Request clientRequest = new Request(Encoding.ASCII.GetString(data, 0, receivedDataLength));
+
+                    // TODO: Call HandleRequest Method that returns the response
+                    Response serverResponse = HandleRequest(clientRequest);
+                    string responseStr = serverResponse.ToString();
+                    byte[] responseArr = Encoding.ASCII.GetBytes(responseStr);
+                    // TODO: Send Response back to client
+                    clientSocket.Send(responseArr);
+
+                }
+                catch (Exception ex)
+                {
+                    // TODO: log exception using Logger class
+                    Logger.LogException(ex);
+                }
+            }
+
+            // TODO: close client socket
+            clientSocket.Close();
+        }
+
+        Response HandleRequest(Request request)
+        {
+            string content;
+            Response response;
+            try
+            {
+                //TODO: check for bad request 
+                //TODO: map the relativeURI in request to get the physical path of the resource.
+                //TODO: check for redirect
+                //TODO: check file exists
+                //TODO: read the physical file
+                // Create OK response
+                if (request.ParseRequest())
+                {
+                    Uri uri = new Uri(request.relativeURI);
+                    string filePath = uri.LocalPath;
+                    string redirectionPath = GetRedirectionPagePathIFExist(filePath);
+                    content = LoadDefaultPage(filePath);
+                    response = new Response(StatusCode.OK, "text/html", content, "");
+                }
+                else
+                {
+                    response = new Response(StatusCode.BadRequest, "text/html", LoadDefaultPage(Configuration.BadRequestDefaultPageName), "");
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: log exception using Logger class
+                // TODO: in case of exception, return Internal Server Error.
+                Logger.LogException(ex);
+                response = new Response(StatusCode.InternalServerError, "text/html", LoadDefaultPage(Configuration.InternalErrorDefaultPageName), "");
+            }
+            return response;
+        }
+
+        private string GetRedirectionPagePathIFExist(string relativePath)
+        {
+            // using Configuration.RedirectionRules return the redirected page path if exists else returns empty
+            LoadRedirectionRules(relativePath);
+            foreach (KeyValuePair<string, string> dict in Configuration.RedirectionRules)
+            {
+                if (dict.Key == relativePath)
+                {
+                    return dict.Value;
+                }
+            }
+            return string.Empty;
+        }
+
+        private string LoadDefaultPage(string defaultPageName)
+        {
+            string filePath = Path.Combine(Configuration.RootPath, defaultPageName);
+            // TODO: check if filepath not exist log exception using Logger class and return empty string
+            if (!Directory.Exists(filePath))
+            {
+                Logger.LogException(new DirectoryNotFoundException());
+                return string.Empty;
+            }
+            // else read file and return its content
+            else
+            {
+                return File.ReadAllText(filePath);
+            }
+        }
+
+        private void LoadRedirectionRules(string filePath)
+        {
+            try
+            {
+                // TODO: using the filepath paramter read the redirection rules from file 
+                // then fill Configuration.RedirectionRules dictionary 
+                Configuration.RedirectionRules = new Dictionary<string, string>();
+                StreamReader reader = new StreamReader(filePath);
+                string content = reader.ReadLine();
+                string[] red = content.Split(',');
+
+                Configuration.RedirectionRules.Add(red[0], red[1]);
+            }
+            catch (Exception ex)
+            {
+                // TODO: log exception using Logger class
+                Logger.LogException(ex);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
